8b8786f0e781797ae089fec0cd5810b211090cf0
Angie <arco527@gmail.com>
2018-12-06 18:58:32 -0500

    Hidden form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
 
+  def create
+    @comment = Comment.new(comments_params)
+    unless @comment.save
+      p "****************"
+      p @comment.errors
+      p "****************"
 
+    end
+  end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:description, :post_id)
+  end
 
 end
